b62b8785a4e090d48608b86254f7dc9ba72d1f81
Zac McCormick <zac.mccormick@gmail.com>
2013-04-10 00:53:38 -0400

    Remove the geometry callback functions
    Removing this because it's not properly supported for dynamic
    extension loading. The 2 SpatiaLite RTree functions require you to
    compile SQLite into the SpatiaLite library which isn't possible with
    the current way this module works.
    
    More info [here](http://trac.osgeo.org/osgeo4w/ticket/305)
    
        SQLite introduced the sqlite3_rtree_geometry_callback() API about
        one or two years ago; so if you are using some very old libsqlite3
        this symbol will not be defined.
    
        Please note: SpatiaLite discourages using the Geometry Callback
        based interface, and will be disabled by default in any new
        version.
        so the suggested solution when building splite is to always define:
    
        DISABLE_GEOMETRY_CALLBACKS
    
        bye Sandro
    
    This reverts commit 589f558710cf323d9c73e57e3f7f1912c9f69395.

--- src/config/win/x64/config-msvc.h
+++ src/config/win/x64/config-msvc.h
@@ -153,7 +153,7 @@
 /* #undef OMIT_FREEXL */
 
 /* Should be defined in order to disable GEOCALLBACKS support. */
-/* #define OMIT_GEOCALLBACKS 1 */
+#define OMIT_GEOCALLBACKS 1
 
 /* Should be defined in order to disable GEOS support. */
 /* #undef OMIT_GEOS */
